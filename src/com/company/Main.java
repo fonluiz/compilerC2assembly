7e9803e4f7f7d03c63902964dd60d1b364bc3efe
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-01-27 19:25:42 -0300

    Cria Main para instanciação de parser a ser utilizado nos testes

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            parser p = new parser(new Lexer(new FileReader(argv[0])));
+            Object result = p.parse().value;
+        } catch (Exception e) {
+            /* do cleanup here -- possibly rethrow e */
+            e.printStackTrace();
+        }
+    }
+}
