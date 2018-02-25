ce6d5293721fa8ca93fca794911f642c062d36d0
fonluiz <luiz.fonseca@ccc.ufcg.edu.br>
2018-02-25 15:31:43 -0300

    ...

--- src/input.c
+++ src/input.c
@@ -1,10 +1,6 @@
-int a=0;
-
-int c;
-
-void main(int d, int e){
-    int f = 3;
-    int h=f, b=0;
-    h + 1; 
-
+int main()
+{
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
 }
