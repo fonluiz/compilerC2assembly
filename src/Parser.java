e7dca0fec9810813a3dc155a2fafe159ef4f4a33
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-02-25 20:09:17 -0300

    Reorganiza repositório e deixa o babado certo

--- src/Parser.java
+++ src/Parser.java
@@ -1500,33 +1500,6 @@ public class Parser extends java_cup.runtime.lr_parser {
 	super(lex,sf);
     }
 
-    public static void main(String args[]) {
-	try {
-	    ComplexSymbolFactory csf = new ComplexSymbolFactory();
-	    // create a buffering scanner wrapper
-	    ScannerBuffer lexer = new ScannerBuffer(new Lexer(new BufferedReader(new FileReader(args[0])),csf));
-	    // start parsing
-	    Parser p = new Parser(lexer,csf);
-	    System.out.println("Parser runs: ");
-	    newScope();
-	    XMLElement e = (XMLElement)p.parse().value;
-	    // create XML output file 
-	    XMLOutputFactory outFactory = XMLOutputFactory.newInstance();
-	    XMLStreamWriter sw = outFactory.createXMLStreamWriter(new FileOutputStream(args[1]));
-	    // dump XML output to the file
-	    XMLElement.dump(lexer,sw,e); //,"expr","stmt");
-	    // transform the parse tree into an AST and a rendered HTML version
-	    Transformer transformer = TransformerFactory.newInstance()
-		.newTransformer(new StreamSource(new File("tree.xsl")));
-	    Source text = new StreamSource(new File(args[1]));
-	    transformer.transform(text, new StreamResult(new File("output.html")));
-
-	    System.out.println("Parsing finished!");
-	} catch (Exception e) {
-	    e.printStackTrace();
-	}
-    }
-
 
 /** Cup generated class to encapsulate user supplied action code.*/
 @SuppressWarnings({"rawtypes", "unchecked", "unused"})
