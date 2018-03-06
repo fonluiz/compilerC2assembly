8b8b8ba27035d698e29736f54f08ce71dddc0ad7
fonluiz <luiz.fonseca@ccc.ufcg.edu.br>
2018-03-06 20:57:29 -0300

    Finalise a análise semantica de operações booleanas

--- src/analysis/exceptions/InvalidBooleanOpException.java
+++ src/analysis/exceptions/InvalidBooleanOpException.java
@@ -3,6 +3,6 @@ package analysis.exceptions;
 public class InvalidBooleanOpException extends Exception{
 
     public InvalidBooleanOpException(String message){
-        super("ERRO SEMÂNTICO - EXPRESSÕES BOOLEANAS: " + message);
+        super("ERRO SEMÂNTICO: " + message);
     }
 }
